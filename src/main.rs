9e096fdc5f37a2144228031fd5be01121330989f
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-03-25 00:38:13 +0100

    Added links to various projects

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use leptos::*;
 
 #[component]
-fn App(cx: Scope) -> impl IntoView { 
-    view!{ cx,
-        <div class="fill center">
-            <h1>"Heavenstone"</h1>
+fn App() -> impl IntoView { 
+    view!{
+        <div class="container">
+            <div class="inner">
+                <h1>"Heavenstone"</h1>
+                <ul>
+                    <li><a href="https://github.com/Heavenston">GitHub</a></li>
+                    <li><a href="https://hfax.fr">Heav-Faxer</a></li>
+                    <li><a href="https://isabellemaire.fr">isabellemaire.fr</a></li>
+                </ul>
+            </div>
         </div>
     }
 }
@@ -13,7 +20,7 @@ fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
 
-    mount_to_body(|cx| view! { cx,
+    mount_to_body(|| view! {
         <App />
     })
 }
