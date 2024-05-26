5c3addb6b189df996a42cb017eee0091a0e56603
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-05-26 16:58:41 +0200

    Changed layout

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,31 @@ use leptos::*;
 fn App() -> impl IntoView { 
     view!{
         <div class="container">
-            <div class="inner">
+            <div class="left">
                 <h1>"Heavenstone"</h1>
                 <ul>
-                    <li><a href="https://github.com/Heavenston/heav_fr_mainpage">GitHub</a></li>
-                    <li><a href="https://hfax.fr">Heav-Faxer</a></li>
-                    <li><a href="https://isabellemaire.fr">isabellemaire.fr</a></li>
-                    <li><a href="https://adhd.heav.fr">ADHD Bubbles</a></li>
+                    <li>
+                        <a href="https://github.com/Heavenston/heav_fr_mainpage">GitHub</a>
+                    </li>
                 </ul>
+            </div>
+            <div class="right">
+                <section>
+                    <h2>"Web"</h2>
+                    <ul>
+                        <li><a href="https://hfax.fr">Heav-Faxer</a></li>
+                        <li><a href="https://isabellemaire.fr">isabellemaire.fr</a></li>
+                        <li><a href="https://adhd.heav.fr">ADHD Bubbles</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <h2>"Autre"</h2>
+                    <ul>
+                        <li><a href="#">Un autre truc</a></li>
+                        <li><a href="#">Encore un autre</a></li>
+                        <li><a href="#">Peut etre different?</a></li>
+                    </ul>
+                </section>
             </div>
         </div>
     }
